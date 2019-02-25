c7e660ba330e97b7b17704c7817562c13c609d31
tengda <kidal@vip.qq.com>
2019-02-25 16:23:17 +0800

    调整

--- services/999-foundation-tests/src/graphql/time.js
+++ services/999-foundation-tests/src/graphql/time.js
@@ -4,15 +4,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const GraphQLUnits_1 = __importDefault(require("../../../../server/graphql/GraphQLUnits"));
+class ServerTime {
+    constructor() {
+        this._now = new Date();
+    }
+    format(args) {
+        return GraphQLUnits_1.default.dateUnit(new Date(), args);
+    }
+    timestamp() {
+        return this._now.getTime();
+    }
+    date() {
+        return GraphQLUnits_1.default.dateUnit(this._now, { unit: "date" });
+    }
+    datetime() {
+        return GraphQLUnits_1.default.dateUnit(this._now, { unit: "datetime" });
+    }
+}
 exports.default = {
     schema: `
 type Query {
-  time(unit: String): String!
+  serverTime: ServerTime!
+}
+
+type ServerTime {
+  format(unit: String): String!
+  timestamp: Float!
+  date: String!
+  datetime: String!
 }
 `,
     resolver: {
-        time(args) {
-            return GraphQLUnits_1.default.dateUnit(new Date(), args);
+        serverTime() {
+            return new ServerTime();
         },
     },
 };
--- services/999-foundation-tests/src/graphql/time.ts
+++ services/999-foundation-tests/src/graphql/time.ts
@@ -1,14 +1,41 @@
 import GraphQLUnits from "../../../../server/graphql/GraphQLUnits";
 
+class ServerTime {
+  private readonly _now = new Date();
+
+  public format(args: any) {
+    return GraphQLUnits.dateUnit(new Date(), args);
+  }
+
+  public timestamp() {
+    return this._now.getTime();
+  }
+
+  public date() {
+    return GraphQLUnits.dateUnit(this._now, {unit: "date"});
+  }
+
+  public datetime() {
+    return GraphQLUnits.dateUnit(this._now, {unit: "datetime"});
+  }
+}
+
 export default {
   schema: `
 type Query {
-  time(unit: String): String!
+  serverTime: ServerTime!
+}
+
+type ServerTime {
+  format(unit: String): String!
+  timestamp: Float!
+  date: String!
+  datetime: String!
 }
 `,
   resolver: {
-    time(args: any) {
-      return GraphQLUnits.dateUnit(new Date(), args);
+    serverTime() {
+      return new ServerTime();
     },
   },
 };
