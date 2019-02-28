495ef5c138a7256cd0346174d5926d31e91e4171
tengda <kidal@vip.qq.com>
2019-02-28 15:51:54 +0800

    单元测试

--- scripts/cleanup_test.sh
+++ scripts/cleanup_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-CI_COMMIT_SHORT_SHA="$1"
+TEST_ID="$1"
 
-mysql -h172.17.56.181 -ugitlab-runner -pgitlab-runner -e'drop database if exists unit_test_'${CI_COMMIT_SHORT_SHA}';'
+mysql -h172.17.56.181 -ugitlab-runner -pgitlab-runner -e'drop database if exists unit_test_'${TEST_ID}';'
--- scripts/prepare_test.sh
+++ scripts/prepare_test.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-CI_COMMIT_SHORT_SHA="$1"
+TEST_ID="$1"
 
-mysql -h172.17.56.181 -ugitlab-runner -pgitlab-runner -e'create database unit_test_'${CI_COMMIT_SHORT_SHA}' default character set utf8mb4 collate utf8mb4_unicode_ci;'
+mysql -h172.17.56.181 -ugitlab-runner -pgitlab-runner -e'create database unit_test_'${TEST_ID}' default character set utf8mb4 collate utf8mb4_unicode_ci;'
 
 sed -i 's/host: 192.168.93.222 \# DATABASE_HOST/host: 172.17.56.181/g' test-services/basic/res/bootstrap-test.yml
 sed -i 's/username: mcg \# DATABASE_USERNAME/username: gitlab-runner/g' test-services/basic/res/bootstrap-test.yml
 sed -i 's/password: Mcg!2345 \# DATABASE_PASSWORD/password: gitlab-runner/g' test-services/basic/res/bootstrap-test.yml
-sed -i 's/database: mcg_games_servers \# DATABASE_DATABASE/database: unit_test_'${CI_COMMIT_SHORT_SHA}'/g' test-services/basic/res/bootstrap-test.yml
+sed -i 's/database: mcg_games_servers \# DATABASE_DATABASE/database: unit_test_'${TEST_ID}'/g' test-services/basic/res/bootstrap-test.yml
