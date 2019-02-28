ca406784bfcdd840d21ae959aed36a81b20885d7
tengda <kidal@vip.qq.com>
2019-02-28 15:57:14 +0800

    单元测试

---
+++ scripts/apply_test_bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TEST_ID="$1"
+
+sed -i 's/host: 192.168.93.222 \# DATABASE_HOST/host: 172.17.56.181/g' test-services/basic/res/bootstrap-test.yml
+sed -i 's/username: mcg \# DATABASE_USERNAME/username: gitlab-runner/g' test-services/basic/res/bootstrap-test.yml
+sed -i 's/password: Mcg!2345 \# DATABASE_PASSWORD/password: gitlab-runner/g' test-services/basic/res/bootstrap-test.yml
+sed -i 's/database: mcg_games_servers \# DATABASE_DATABASE/database: unit_test_'${TEST_ID}'/g' test-services/basic/res/bootstrap-test.yml
