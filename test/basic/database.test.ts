4705d0c6e1712a865ee059592b672bb3536a25be
tengda <kidal@vip.qq.com>
2019-03-04 17:40:37 +0800

    强化数据库

--- test/basic/database.test.ts
+++ test/basic/database.test.ts
@@ -5,10 +5,10 @@ import Database from "../../src/data/Database";
 
 describe("Basic: IEntityCache", () => {
   // noinspection TsLint
-  let UserCache: IEntityCache<number, IUser>
+  let userCache: IEntityCache<number, IUser>
   beforeAll(async () => {
     await Application.runTest("basic");
-    UserCache = Database.S.getCache("999_user")
+    userCache = Database.S.getCache("999_user")
   })
 
   afterAll(async () => {
@@ -16,59 +16,59 @@ describe("Basic: IEntityCache", () => {
   })
 
   it("#get", async () => {
-    const user = await UserCache.get(1)
+    const user = await userCache.get(1)
     expect(user).toBeNull()
   });
 
   it("#getOrCreate", async () => {
-    const user1 = await UserCache.getOrCreate(1, {username: "sa", password: "sa"});
+    const user1 = await userCache.getOrCreate(1, {username: "sa", password: "sa"});
     expect(user1).not.toBeNull()
 
-    const user2 = await UserCache.getOrCreate(1, {username: "sa", password: "sa"});
+    const user2 = await userCache.getOrCreate(1, {username: "sa", password: "sa"});
     expect(user2).not.toBeNull()
   })
 
   it("#loadOne", async () => {
-    const user = await UserCache.loadOne(async (model) => {
+    const user = await userCache.loadOne(async (model) => {
       return model.findOne({where: {username: "sa"}});
     })
     expect(user).not.toBeNull()
   });
 
   it("#loadMany", async () => {
-    const user = await UserCache.loadMany(async (model) => {
+    const user = await userCache.loadMany(async (model) => {
       return model.findAll({where: {username: "sa"}});
     })
     expect(user).toHaveLength(1)
   });
 
   it("#create", async () => {
-    const user = await UserCache.create({username: "sa2", password: "sa"});
+    const user = await userCache.create({username: "sa2", password: "sa"});
     expect(user).not.toBeNull()
   });
 
   it("#createOrUpdate", async () => {
-    const user = await UserCache.createOrUpdate({id: 2, username: "sa2", password: "sa2"});
+    const user = await userCache.createOrUpdate({id: 2, username: "sa2", password: "sa2"});
     expect(user).not.toBeNull()
   });
 
   it("#updateOne", async () => {
-    const user = await UserCache.updateOne({id: 2, username: "sa3", password: "sa2"});
+    const user = await userCache.updateOne({id: 2, username: "sa3", password: "sa2"});
     expect(user).not.toBeNull()
 
-    const user2 = await UserCache.get(2)
+    const user2 = await userCache.get(2)
     expect(user2).not.toBeNull()
     expect(user2!.username).toEqual("sa3")
   });
 
   it("AutoUpdateChangedFields", async () => {
-    const user = await UserCache.get(2)
+    const user = await userCache.get(2)
     expect(user).not.toBeNull()
 
     user!.username = "autoUpdate"
     await user!.waitAutoUpdateChangedFieldsComplete()
 
-    const updated = await UserCache.get(2)
+    const updated = await userCache.get(2)
     expect(updated).not.toBeNull()
     expect(updated!.username).toEqual("autoUpdate")
   })
