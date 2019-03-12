29ada2b8380c260bae41f12a2f3fdfa19e0c2321
tengda <kidal@vip.qq.com>
2019-03-12 14:24:52 +0800

    GraphQL注释

--- src/server/graphql/GraphQLUtils.ts
+++ src/server/graphql/GraphQLUtils.ts
@@ -4,37 +4,43 @@ import Maybe from "graphql/tsutils/Maybe";
 export default {
   makeConnectionSchema(name: string) {
     return `type ${name}Edge {
-	# 节点
+	"节点"
 	node: ${name}
-	# 当前节点游标
+	"当前节点游标"
 	cursor: String
 }
 
 type ${name}Connection {
-	# 节点列表
+	"节点列表"
 	nodes: [${name}!]!
-	# 边列表
+	"边列表"
 	edges: [${name}Edge!]!
-	# 全部元素个数
+	"全部节点个数"
 	totalCount: Int!
-	# 向前分页起始游标
+	"向前分页起始游标"
 	startCursor: String
-	# 向后分页起始游标
+	"向后分页起始游标"
 	endCursor: String
-	# 是否有下一页
+	"是否有下一页"
 	hasNextPage: Boolean!
-	# 是否有上一页
+	"是否有上一页"
 	hasPreviousPage: Boolean!
-	# 当前分页参数
+	"当前分页参数"
 	pageArgs: ${name}PageArgs!
 }
 
 type ${name}PageArgs {
+  "页码"
   page: Int
+  "每页个数"
   limit: Int
+  "after后多少个"
   first: Int
+  "起始游标"
   after: String
+  "before前多少个"
   last: Int
+  "结束游标"
   before: String
 }`
   },
