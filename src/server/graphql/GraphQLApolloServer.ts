f8d61b1f8c39606d4c5bad88f32d84f95f31df9c
tengda <kidal@vip.qq.com>
2019-03-15 13:55:10 +0800

    使用 Apollo GraphQL 替代 GraphQL Express

---
+++ src/server/graphql/GraphQLApolloServer.ts
@@ -0,0 +1,120 @@
+import {ApolloServer, ApolloServerExpressConfig, ServerRegistration} from "apollo-server-express";
+import Cors from "cors"
+import Accepts from "accepts"
+// @ts-ignore
+import TypeIs from "type-is"
+import PayloadDispatcher from "../PayloadDispatcher";
+import expressApollo = require("apollo-server-express/dist/expressApollo");
+import BodyParser = require("body-parser");
+import ApolloServerCore = require("apollo-server-core");
+import GraphqlPlaygroundHtml = require( "@apollographql/graphql-playground-html");
+
+const fileUploadMiddleware = (uploadsConfig: any, server: any) => (req: any, res: any, next: any) => {
+  if (typeof ApolloServerCore.processFileUploads === "function" &&
+    TypeIs(req, ["multipart/form-data"])) {
+    // @ts-ignore
+    ApolloServerCore.processFileUploads(req, res, uploadsConfig)
+      .then((body) => {
+        req.body = body;
+        next();
+      })
+      .catch((error) => {
+        if (error.status && error.expose) {
+          res.status(error.status);
+        }
+        next(ApolloServerCore.formatApolloErrors([error], {
+          formatter: server.requestOptions.formatError,
+          debug: server.requestOptions.debug,
+        }));
+      });
+  } else {
+    next();
+  }
+};
+
+/**
+ * @author tengda
+ */
+export default class GraphQLApolloServer extends ApolloServer {
+  constructor(config: ApolloServerExpressConfig) {
+    super(config);
+  }
+
+  public applyMiddleware(
+    {
+      app,
+      path,
+      cors,
+      bodyParserConfig,
+      disableHealthCheck,
+      onHealthCheck,
+    }: ServerRegistration,
+  ): void {
+    if (!path) {
+      path = "/graphql"
+    }
+    const promiseWillStart = this.willStart()
+    app.use(path, (_req, _res, next) => {
+      promiseWillStart.then(() => next()).catch(next);
+    });
+    if (!disableHealthCheck) {
+      app.use("/.well-known/apollo/server-health", (req, res) => {
+        res.type("application/health+json");
+        if (onHealthCheck) {
+          onHealthCheck(req)
+            .then(() => {
+              res.json({status: "pass"});
+            })
+            .catch(() => {
+              res.status(503).json({status: "fail"});
+            });
+        } else {
+          res.json({status: "pass"});
+        }
+      });
+    }
+    let uploadsMiddleware;
+    if (this.uploadsConfig && typeof ApolloServerCore.processFileUploads === "function") {
+      uploadsMiddleware = fileUploadMiddleware(this.uploadsConfig, this);
+    }
+    this.graphqlPath = path;
+    if (cors === true) {
+      app.use(path, Cors());
+    } else if (cors !== false) {
+      app.use(path, Cors(cors));
+    }
+    if (bodyParserConfig === true) {
+      app.use(path, BodyParser.json());
+    } else if (bodyParserConfig !== false) {
+      app.use(path, BodyParser.json(bodyParserConfig));
+    }
+    if (uploadsMiddleware) {
+      app.use(path, uploadsMiddleware);
+    }
+    app.use(path, (req, res, next) => {
+      if (this.playgroundOptions && req.method === "GET") {
+        const accept = Accepts(req);
+        const types = accept.types();
+        const prefersHTML = (types as string[]).find(
+          (x: string) => x === "text/html" || x === "application/json") === "text/html";
+        if (prefersHTML) {
+          const playgroundRenderPageOptions = Object.assign({
+            endpoint: path,
+            subscriptionEndpoint: this.subscriptionsPath,
+          }, this.playgroundOptions);
+          res.setHeader("Content-Type", "text/html");
+          const playground = GraphqlPlaygroundHtml.renderPlaygroundPage(playgroundRenderPageOptions);
+          res.write(playground);
+          res.end();
+          return;
+        }
+      }
+
+      return PayloadDispatcher.S.dispatchGraphQL(() => {
+        expressApollo.graphqlExpress(() => {
+          return this.createGraphQLServerOptions(req, res);
+        })(req, res, next)
+      })
+    });
+  }
+}
--- src/server/graphql/GraphQLServer.ts
+++ src/server/graphql/GraphQLServer.ts
@@ -1,14 +1,14 @@
-import {buildSchema, formatError} from "graphql";
+import {formatError} from "graphql";
 import HttpServer from "../HttpServer";
-import graphqlHTTP from "express-graphql";
 import glob from "glob";
-import {mergeResolvers, mergeTypes} from "merge-graphql-schemas";
 import Environment from "../../application/Environment";
 import Logs from "../../application/Logs";
 import LudmilaError from "../../error/LudmilaError";
 import LudmilaErrors from "../../error/LudmilaErrors";
-import {Request, Response} from "express";
+import GraphQLApolloServer from "./GraphQLApolloServer";
+import {GraphQLExtension} from "graphql-extensions";
 import PayloadDispatcher from "../PayloadDispatcher";
+import {makeExecutableSchema, mergeSchemas} from "graphql-tools";
 
 /**
  * @author tengda
@@ -32,18 +32,21 @@ export default class GraphQLServer {
     const env = Environment.S;
     const config = env.applicationConfig.server.graphQL;
 
-    // 读取格式和解析器
-    const {schema, resolvers} = this.loadSchemaAndResolvers(env);
-    if (schema === null || resolvers === null) {
-      GraphQLServer.LOG.info("No graphql autowired");
-      return;
-    }
+    // 加载注册列表
+    const registryList = glob
+      .sync(`${env.srcDir}/**/graphql/**/*.js`)
+      .map((it: string) => require(it).default)
+      .filter((it: any) => !!it);
+    const schemaList = registryList
+      .map((it: any) => makeExecutableSchema({
+        typeDefs: it.schema,
+        resolvers: it.resolvers,
+      }))
+    const schema = mergeSchemas({schemas: schemaList})
 
-    // GraphQL终端
-    const graphQLMiddleware = graphqlHTTP({
+    // 使用Apollo中间价
+    const apolloServer = new GraphQLApolloServer({
       schema,
-      rootValue: resolvers,
-      graphiql: true,
       formatError: (error) => {
         const formattedError = formatError(error);
         const originalError = error.originalError;
@@ -53,37 +56,47 @@ export default class GraphQLServer {
           return Object.assign({}, formattedError, {code: LudmilaErrors.FAIL});
         }
       },
-      extensions: () => {
-        return {foundation: {sync: PayloadDispatcher.S.getSync()}};
+      extensions: [
+        (): GraphQLExtension => ({
+          willSendResponse: ({graphqlResponse}) => {
+            const extensions = graphqlResponse.extensions || {}
+            extensions.foundation = {sync: PayloadDispatcher.S.getSync()}
+            graphqlResponse.extensions = extensions
+          },
+        }),
+      ],
+      playground: {
+        settings: {
+          "editor.cursorShape": "line", // possible values: 'line', 'block', 'underline'
+          "editor.fontFamily": `'Monaco', monospace, 'Consolas'`,
+          "editor.fontSize": 18,
+          "editor.reuseHeaders": true, // new tab reuses headers from last tab
+          "editor.theme": "dark", // possible values: 'dark', 'light'
+          "general.betaUpdates": false,
+          "prettier.printWidth": 80,
+          "prettier.tabWidth": 2,
+          "prettier.useTabs": false,
+          "request.credentials": "include", // possible values: 'omit', 'include', 'same-origin'
+          "schema.polling.enable": true, // enables automatic schema polling
+          "schema.polling.endpointFilter": "*localhost*", // endpoint filter for schema polling
+          "schema.polling.interval": 2000, // schema polling interval in ms
+          "schema.disableComments": false,
+          "tracing.hideTracingResponse": true,
+        },
       },
     });
-    httpServer.expressApp.use(config.endpoint, async (request: Request, response: Response): Promise<undefined> => {
-      return PayloadDispatcher.S.dispatchGraphQL(graphQLMiddleware, request, response);
-    });
+    apolloServer.applyMiddleware({
+      app: httpServer.expressApp,
+      path: config.endpoint,
+      cors: {
+        origin: true,
+        allowedHeaders: "*",
+        methods: "*",
+        credentials: true,
+      },
+    })
 
     // log
     GraphQLServer.LOG.info(`Using GraphQL at endpoint ${config.endpoint}`);
-  }
-
-  private loadSchemaAndResolvers(env: Environment) {
-    const pieces = glob
-      .sync(`${env.srcDir}/**/graphql/**/*.js`)
-      .map((it: string) => require(it).default)
-      .filter((it: any) => !!it);
-    if (pieces.length === 0) {
-      return {schema: null, resolvers: null};
-    }
-
-    const source = mergeTypes(
-      pieces.map((it: any) => it.schema).filter((it: any) => !!it),
-      {all: true},
-    );
-    // GraphQLServer.LOG.debug(`Merged Schema\n${source}`);
-    const schema = buildSchema(source, {commentDescriptions: true});
-    const resolvers = mergeResolvers(
-      pieces.map((it: any) => it.resolver).filter((it: any) => !!it),
-    );
-
-    return {schema, resolvers};
   }
 }
