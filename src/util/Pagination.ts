bb53bb3bc1c9d131092c0fd81757f270527572ad
tengda <kidal@vip.qq.com>
2019-08-08 13:13:46 +0800

    Release 0.9.13

--- src/util/Pagination.ts
+++ src/util/Pagination.ts
@@ -136,7 +136,7 @@ export class Connection<TNode> {
   /**
    * @author tengda
    */
-  public static async empty<TNode>(pageArgs?: PageArgs): Promise<Connection<TNode>> {
+  public static empty<TNode>(pageArgs?: PageArgs): Connection<TNode> {
     return new Connection<TNode>([], 0, null, null, pageArgs || PageArgs.page())
   }
 
