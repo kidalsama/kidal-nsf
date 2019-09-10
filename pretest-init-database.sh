cb2c235cab6b1e66d260c9582b660e3aa72c165e
tengda <kidal@vip.qq.com>
2019-09-10 18:39:43 +0800

    feature(Data): database

--- pretest-init-database.sh
+++ pretest-init-database.sh
@@ -7,6 +7,3 @@ mysql -h39.107.15.85 -umcg -p'Mcg!2345' -e'drop database if exists test_nsf_2;'
 # 建库
 mysql -h39.107.15.85 -umcg -p'Mcg!2345' -e'create database test_nsf_1 default character set utf8mb4 collate utf8mb4_unicode_ci;'
 mysql -h39.107.15.85 -umcg -p'Mcg!2345' -e'create database test_nsf_2 default character set utf8mb4 collate utf8mb4_unicode_ci;'
-
-# 测试
-yarn run test
