9f6bf33fa16de899358a55c3cea9829a473551af
tengda <kidal@vip.qq.com>
2020-02-23 19:07:08 +0800

    Release 0.18.8

--- src/util/Pagination.ts
+++ src/util/Pagination.ts
@@ -1,28 +1,28 @@
 /**
  * @author tengda
  */
-export const DEFAULT_PAGE = 1
+export const DEFAULT_PAGE = 1;
 
 /**
  * @author tengda
  */
-export const DEFAULT_LIMIT = 20
+export const DEFAULT_LIMIT = 20;
 
 /**
  * @author tengda
  */
-export const MAX_LIMIT = 1000
+export const MAX_LIMIT = 1000;
 
 /**
  * @author tengda
  */
 export class PageArgs {
-  public readonly page: number
-  public readonly limit: number
-  public readonly first: number
-  public readonly after: string | null
-  public readonly last: number
-  public readonly before: string | null
+  public readonly page: number;
+  public readonly limit: number;
+  public readonly first: number;
+  public readonly after: string | null;
+  public readonly last: number;
+  public readonly before: string | null;
 
   /**
    * @author tengda
@@ -33,49 +33,49 @@ export class PageArgs {
     first: number = DEFAULT_LIMIT,
     after: string | null = null,
     last: number = DEFAULT_LIMIT,
-    before: string | null = null,
+    before: string | null = null
   ) {
-    this.page = page
-    this.limit = limit
-    this.first = first
-    this.after = after
-    this.last = last
-    this.before = before
+    this.page = page;
+    this.limit = limit;
+    this.first = first;
+    this.after = after;
+    this.last = last;
+    this.before = before;
   }
 
   /**
    * @author tengda
    */
   public static zero() {
-    return new PageArgs(DEFAULT_PAGE, 0)
+    return new PageArgs(DEFAULT_PAGE, 0);
   }
 
   /**
    * @author tengda
    */
   public static one() {
-    return new PageArgs(DEFAULT_PAGE, 1)
+    return new PageArgs(DEFAULT_PAGE, 1);
   }
 
   /**
    * @author tengda
    */
   public static max() {
-    return new PageArgs(DEFAULT_PAGE, MAX_LIMIT)
+    return new PageArgs(DEFAULT_PAGE, MAX_LIMIT);
   }
 
   /**
    * @author tengda
    */
   public static page(page: number = DEFAULT_PAGE, limit: number = MAX_LIMIT) {
-    return new PageArgs(page, limit)
+    return new PageArgs(page, limit);
   }
 
   /**
    * 起始位置
    */
   public get offset(): number {
-    return (this.page - 1) * this.limit
+    return (this.page - 1) * this.limit;
   }
 }
 
@@ -83,19 +83,19 @@ export class PageArgs {
  * @author tengda
  */
 export interface IEdge<TNode> {
-  node?: TNode
-  cursor?: string
+  node?: TNode;
+  cursor?: string;
 }
 
 /**
  * @author tengda
  */
 export class Connection<TNode> {
-  public readonly edges: Array<IEdge<TNode>>
-  public readonly totalCount: number
-  public readonly startCursor: string | null
-  public readonly endCursor: string | null
-  public readonly pageArgs: PageArgs
+  public readonly edges: Array<IEdge<TNode>>;
+  public readonly totalCount: number;
+  public readonly startCursor: string | null;
+  public readonly endCursor: string | null;
+  public readonly pageArgs: PageArgs;
 
   /**
    * @author tengda
@@ -105,13 +105,13 @@ export class Connection<TNode> {
     totalCount: number,
     startCursor: string | null,
     endCursor: string | null,
-    pageArgs: PageArgs,
+    pageArgs: PageArgs
   ) {
-    this.edges = edges
-    this.totalCount = totalCount
-    this.startCursor = startCursor
-    this.endCursor = endCursor
-    this.pageArgs = pageArgs
+    this.edges = edges;
+    this.totalCount = totalCount;
+    this.startCursor = startCursor;
+    this.endCursor = endCursor;
+    this.pageArgs = pageArgs;
   }
 
   /**
@@ -120,45 +120,65 @@ export class Connection<TNode> {
   public static async make<TNode>(
     nodes: TNode[] | (() => Promise<TNode[]>),
     totalCount: number | (() => Promise<number>),
-    pageArgs: PageArgs,
+    pageArgs: PageArgs
   ): Promise<Connection<TNode>> {
-    const fixedTotalCount: number = typeof totalCount === "function"
-      ? await totalCount() : totalCount
-    const fixedNodes: TNode[] = pageArgs.limit > 0
-      ? (typeof nodes === "function" ? await nodes() : nodes) : []
+    const fixedTotalCount: number =
+      typeof totalCount === "function" ? await totalCount() : totalCount;
+    const fixedNodes: TNode[] =
+      pageArgs.limit > 0
+        ? typeof nodes === "function"
+          ? await nodes()
+          : nodes
+        : [];
 
     return new Connection<TNode>(
-      fixedNodes.map((it) => ({node: it, cursor: undefined})),
-      fixedTotalCount, null, null, pageArgs,
-    )
+      fixedNodes.map(it => ({ node: it, cursor: undefined })),
+      fixedTotalCount,
+      null,
+      null,
+      pageArgs
+    );
   }
 
   /**
    * @author tengda
    */
   public static empty<TNode>(pageArgs?: PageArgs): Connection<TNode> {
-    return new Connection<TNode>([], 0, null, null, pageArgs || PageArgs.page())
+    return new Connection<TNode>(
+      [],
+      0,
+      null,
+      null,
+      pageArgs || PageArgs.page()
+    );
   }
 
   /**
    * 全部节点
    */
   public get nodes(): TNode[] {
-    return this.edges.map((it) => it.node!)
+    return this.edges.map(it => it.node!);
+  }
+
+  /**
+   * 第一个节点
+   */
+  public get firstOrNull(): TNode | null {
+    return this.edges.length > 0 ? this.edges[0].node || null : null;
   }
 
   /**
    * 是否有下一页
    */
   public get hasNextPage(): boolean {
-    return this.totalCount > (this.pageArgs.page * this.pageArgs.limit)
+    return this.totalCount > this.pageArgs.page * this.pageArgs.limit;
   }
 
   /**
    * 是否有上一页
    */
   public get hasPreviousPage(): boolean {
-    return this.pageArgs.page > 1
+    return this.pageArgs.page > 1;
   }
 
   /**
@@ -166,17 +186,17 @@ export class Connection<TNode> {
    */
   public map<R>(transform: (node: TNode) => R): Connection<R> {
     return new Connection<R>(
-      this.edges.map((it) => {
+      this.edges.map(it => {
         return {
           node: it.node ? transform(it.node) : undefined,
-          cursor: it.cursor,
-        }
+          cursor: it.cursor
+        };
       }),
       this.totalCount,
       this.startCursor,
       this.endCursor,
-      this.pageArgs,
-    )
+      this.pageArgs
+    );
   }
 
   /**
@@ -188,15 +208,15 @@ export class Connection<TNode> {
       totalCount: this.totalCount,
       startCursor: this.startCursor,
       endCursor: this.endCursor,
-      pageArgs: this.pageArgs,
-    })
+      pageArgs: this.pageArgs
+    });
   }
 
   /**
    * 从Json解析
    */
   public static parse<T>(s: any): Connection<T> {
-    const obj = typeof s === "string" ? JSON.parse(s) : s
+    const obj = typeof s === "string" ? JSON.parse(s) : s;
 
     return new Connection<T>(
       obj.edges,
@@ -209,8 +229,8 @@ export class Connection<TNode> {
         obj.pageArgs.first,
         obj.pageArgs.after,
         obj.pageArgs.last,
-        obj.pageArgs.before,
-      ),
-    )
+        obj.pageArgs.before
+      )
+    );
   }
 }
