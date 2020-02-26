2d08d23ec8a97b863d52d43d338d24972aef0c53
tengda <kidal@vip.qq.com>
2020-02-26 11:11:50 +0800

    Release 0.18.9

--- src/data/DatabaseManager.ts
+++ src/data/DatabaseManager.ts
@@ -1,12 +1,12 @@
-import {Autowired, Service} from "../ioc/Annotation";
+import { Autowired, Service } from "../ioc/Annotation";
 import Logs from "../application/Logs";
 import Database from "./Database";
 import Environment from "../application/Environment";
-import {IDatabaseConfig} from "../application/ApplicationConfig";
+import { IDatabaseConfig } from "../application/ApplicationConfig";
 import Sequelize from "sequelize";
 import PathUtils from "../util/PathUtils";
 import * as fs from "fs";
-import {IDatabaseInitializer} from "./IDatabaseInitializer";
+import { IDatabaseInitializer } from "./IDatabaseInitializer";
 
 /**
  * @author tengda
@@ -16,29 +16,29 @@ export class DatabaseManager {
   /**
    * 日志
    */
-  private static readonly LOG = Logs.S.getFoundationLogger(__dirname, "DatabaseManager");
+  private static readonly LOG = Logs.S.getFoundationLogger(
+    __dirname,
+    "DatabaseManager"
+  );
   /**
    * 数据库列表
    */
-  private readonly databaseMap: Map<string, Database> = new Map()
+  private readonly databaseMap: Map<string, Database> = new Map();
 
   /**
    *
    */
-  public constructor(
-    @Autowired public readonly env: Environment,
-  ) {
-  }
+  public constructor(@Autowired public readonly env: Environment) {}
 
   /**
    * 获取数据库
    */
   public acquire(name: string = "primary"): Database {
-    const database = this.databaseMap.get(name)
+    const database = this.databaseMap.get(name);
     if (database === undefined) {
-      throw new Error(`Database ${name} not found`)
+      throw new Error(`Database ${name} not found`);
     }
-    return database
+    return database;
   }
 
   /**
@@ -46,58 +46,65 @@ export class DatabaseManager {
    */
   public async boot(): Promise<DatabaseManager> {
     // 检查是否开启数据库功能
-    const config = this.env.applicationConfig.data
+    const config = this.env.applicationConfig.data;
     if (!config.enabled) {
-      return this
+      return this;
     }
 
     // 读取初始化器
-    const initializerSrc = PathUtils.path.join(this.env.srcDir, "DatabaseInitializer.ts")
+    const initializerSrc = PathUtils.path.join(
+      this.env.srcDir,
+      "DatabaseInitializer.ts"
+    );
     const initializer: IDatabaseInitializer = fs.existsSync(initializerSrc)
       ? require(PathUtils.replaceExt(initializerSrc, ".js")).default
-      : {}
+      : {};
 
     // 获取全部数据库名
-    const names = Object.keys(config.databaseMap)
+    const names = Object.keys(config.databaseMap);
 
     // 这里要先添加
     // 初始化时扫描文件会读取这里的数据库
     for (const name of names) {
       // 载入配置
-      const databaseConfig = this.loadDatabaseConfig(name)
+      const databaseConfig = this.loadDatabaseConfig(name);
 
       // 创建Sequelize并测试连接
-      const sequelize = this.createSequelize(databaseConfig)
-      await sequelize.authenticate()
+      const sequelize = this.createSequelize(databaseConfig);
+      await sequelize.authenticate();
 
       // 日志
       if (DatabaseManager.LOG.isInfoEnabled()) {
-        const options = sequelize.options
+        const options = sequelize.options;
         DatabaseManager.LOG.info(
-          `Connect database ${options.dialect}://${options.host}:${options.port}/${options.database} successful`)
+          `Connect database ${options.dialect}://${options.host}:${options.port}/${options.database} successful`
+        );
       }
 
       // 添加到字典
-      this.databaseMap.set(name, new Database(this.env, name, databaseConfig, sequelize))
+      this.databaseMap.set(
+        name,
+        new Database(this.env, name, databaseConfig, sequelize)
+      );
     }
 
     // 扫描实体和数据模型
     for (const database of this.databaseMap.values()) {
-      await database.scanEntities()
+      await database.scanEntities();
     }
 
     // 升级
     for (const database of this.databaseMap.values()) {
-      await database.migrateUp(initializer)
+      await database.migrateUp(initializer);
     }
 
     // 初始化
     for (const database of this.databaseMap.values()) {
-      await database.initializeData()
+      await database.initializeData();
     }
 
     // done
-    return this
+    return this;
   }
 
   /**
@@ -105,16 +112,16 @@ export class DatabaseManager {
    */
   public async shutdownAll(): Promise<void> {
     // 检查是否开启数据库功能
-    const config = this.env.applicationConfig.data
+    const config = this.env.applicationConfig.data;
     if (!config.enabled) {
-      return
+      return;
     }
 
     for (const database of this.databaseMap.values()) {
       try {
-        await database.shutdown()
+        await database.shutdown();
       } catch (e) {
-        DatabaseManager.LOG.warn(e)
+        DatabaseManager.LOG.warn(e);
       }
     }
   }
@@ -123,15 +130,15 @@ export class DatabaseManager {
    * 获取数据库配置
    */
   private loadDatabaseConfig(name: string): IDatabaseConfig {
-    const databaseMap = this.env.applicationConfig.data.databaseMap
+    const databaseMap = this.env.applicationConfig.data.databaseMap;
     let databaseConfig: IDatabaseConfig = databaseMap[name];
     if (!databaseConfig) {
-      throw new Error(`Database config ${name} not found`)
+      throw new Error(`Database config ${name} not found`);
     }
     if (databaseConfig.alias) {
-      databaseConfig = this.loadDatabaseConfig(databaseConfig.alias)
+      databaseConfig = this.loadDatabaseConfig(databaseConfig.alias);
     }
-    return databaseConfig
+    return databaseConfig;
   }
 
   /**
@@ -139,7 +146,7 @@ export class DatabaseManager {
    */
   public createSequelize(
     config: IDatabaseConfig,
-    logFunc?: (message: any) => void,
+    logFunc?: (message: any) => void
   ): Sequelize.Sequelize {
     return new Sequelize({
       host: config.host,
@@ -147,11 +154,12 @@ export class DatabaseManager {
       username: config.username,
       password: config.password,
       database: config.database,
-      timezone: config.timezone,
+      timezone:
+        config.timezone === "Asia/Shanghai" ? "+08:00" : config.timezone,
       dialect: config.dialect,
       dialectOptions: {
         supportBigNumbers: true,
-        bigNumberStrings: true,
+        bigNumberStrings: true
       },
       operatorsAliases: {
         $eq: Sequelize.Op.eq,
@@ -188,30 +196,30 @@ export class DatabaseManager {
         $all: Sequelize.Op.all,
         $values: Sequelize.Op.values,
         $col: Sequelize.Op.col,
-        $raw: Sequelize.Op.raw,
+        $raw: Sequelize.Op.raw
       },
       define: {
         charset: "utf8mb4",
         collate: "utf8mb4_unicode_ci",
         engine: "InnoDB",
-        freezeTableName: true,
+        freezeTableName: true
       },
       query: {
-        raw: true,
+        raw: true
       },
       pool: {
         max: 10,
         min: 0,
         acquire: 5 * 1000, // 等待多少毫秒后拿不到连接抛异常
-        idle: 10 * 1000, // 最长线程空闲时间
+        idle: 10 * 1000 // 最长线程空闲时间
       },
       logging: (message: any) => {
         if (logFunc) {
-          logFunc(message)
+          logFunc(message);
         } else {
-          DatabaseManager.LOG.trace(message)
+          DatabaseManager.LOG.trace(message);
         }
-      },
-    })
+      }
+    });
   }
 }
