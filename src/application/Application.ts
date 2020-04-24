a2fdd0a3165366738cd40662f1a4a81976f8345c
tengda <kidal@vip.qq.com>
2020-04-24 10:25:06 +0800

    feat(Application): 优化入口文件

--- src/application/Application.ts
+++ src/application/Application.ts
@@ -3,15 +3,15 @@ import Environment from "./Environment";
 import Logs from "./Logs";
 import DiscoveryClient from "../cluster/DiscoveryClient";
 import Rpc from "../cluster/Rpc";
-import {applicationBanner} from "./ApplicationConstants";
+import { applicationBanner } from "./ApplicationConstants";
 import RpcApiManager from "../cluster/RpcApiManager";
 import * as fs from "fs";
-import {Service} from "../ioc/Annotation";
-import {HttpServerManager} from "../server/HttpServerManager";
+import { Service } from "../ioc/Annotation";
+import { HttpServerManager } from "../server/HttpServerManager";
 import PathUtils from "../util/PathUtils";
-import {DatabaseManager} from "../data/DatabaseManager";
-import {Container} from "../ioc/Container";
-import {Scope} from "../ioc/Scope";
+import { DatabaseManager } from "../data/DatabaseManager";
+import { Container } from "../ioc/Container";
+import { Scope } from "../ioc/Scope";
 
 /**
  * @author tengda
@@ -22,7 +22,7 @@ export default class Application {
    * 单例
    */
   public static get S(): Application {
-    return Container.get(Application)
+    return Container.get(Application);
   }
 
   /**
@@ -40,7 +40,7 @@ export default class Application {
     console.log(applicationBanner);
 
     // 开始启动流程
-    return this.startBootstrapSequence(argv, false)
+    return this.startBootstrapSequence(argv, false);
   }
 
   /**
@@ -48,42 +48,47 @@ export default class Application {
    * @param serviceName 服务名
    * @param applicationConfigType 应用配置类型
    */
-  public static async runTest(serviceName: string, applicationConfigType: string = "test"): Promise<Application> {
+  public static async runTest(
+    serviceName: string,
+    applicationConfigType: string = "test"
+  ): Promise<Application> {
     // 伪造命令行参数
-    const argv = ["", "", serviceName, applicationConfigType]
+    const argv = ["", "", serviceName, applicationConfigType];
 
     // 开始启动流程
-    return this.startBootstrapSequence(argv, true)
+    return this.startBootstrapSequence(argv, true);
   }
 
   // 开始启动流程
-  private static async startBootstrapSequence(argv: string[], testing: boolean): Promise<Application> {
+  private static async startBootstrapSequence(
+    argv: string[],
+    testing: boolean
+  ): Promise<Application> {
     // timer
     const startTs = Date.now();
 
     // 绑定类型
-    Container
-      .bind(Environment)
+    Container.bind(Environment)
       .scope(Scope.SINGLETON)
       .provider({
         get() {
-          return new Environment(argv, testing)
+          return new Environment(argv, testing);
         },
-      })
+      });
     Container.bind(Logs)
       .scope(Scope.SINGLETON)
       .provider({
         get() {
-          return new Logs(Container.get(Environment))
+          return new Logs(Container.get(Environment));
         },
-      })
+      });
 
     // 环境
-    const env = Container.get(Environment)
-    await env.boot()
+    const env = Container.get(Environment);
+    await env.boot();
 
     // 日志初始化
-    const logs = Container.get(Logs)
+    const logs = Container.get(Logs);
     this.LOG = logs.getFoundationLogger(__dirname, "Application");
 
     // 启动应用程序
@@ -93,7 +98,9 @@ export default class Application {
     const sec = ((Date.now() - startTs) / 1000).toFixed(3);
 
     // log
-    Application.LOG.info(`Started ${env.applicationConfig.id} in ${sec} seconds`);
+    Application.LOG.info(
+      `Started ${env.applicationConfig.id} in ${sec} seconds`
+    );
 
     // 搞定
     return this.S;
@@ -102,12 +109,12 @@ export default class Application {
   /**
    * 数据库管理器
    */
-  private databaseManager: DatabaseManager
+  private databaseManager: DatabaseManager;
 
   /**
    * Http服务器管理器
    */
-  private httpServerManager: HttpServerManager
+  private httpServerManager: HttpServerManager;
 
   /**
    * 构造方法
@@ -119,10 +126,10 @@ export default class Application {
   // 启动应用
   private async boot(env: Environment) {
     // 启动数据库
-    this.databaseManager = await Container.get(DatabaseManager).boot()
+    this.databaseManager = await Container.get(DatabaseManager).boot();
 
     // 启动服务器
-    this.httpServerManager = await Container.get(HttpServerManager).boot()
+    this.httpServerManager = await Container.get(HttpServerManager).boot();
 
     // 启动发现服务
     await DiscoveryClient.S.init();
@@ -132,9 +139,12 @@ export default class Application {
     await Rpc.S.init();
 
     // 加载入口
-    const entrySrc = PathUtils.path.join(env.srcDir, "Entry.ts")
+    const entrySrc = PathUtils.path.join(env.srcDir, "Entry.ts");
     if (fs.existsSync(entrySrc)) {
-      require(PathUtils.replaceExt(entrySrc, ".js"))
+      const main = require(PathUtils.replaceExt(entrySrc, ".js")).default;
+      if (typeof main === "function") {
+        await main();
+      }
     }
   }
 
@@ -144,16 +154,16 @@ export default class Application {
   public async shutdown() {
     // 在测试模式延迟关闭
     if (Environment.S.testing) {
-      await new Promise((resolve) => setTimeout(resolve, 1000))
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
 
     // 关闭服务器
-    await this.httpServerManager.shutdownAll()
+    await this.httpServerManager.shutdownAll();
 
     // 关闭发现服务
-    await DiscoveryClient.S.shutdown()
+    await DiscoveryClient.S.shutdown();
 
     // 关闭数据库
-    await this.databaseManager.shutdownAll()
+    await this.databaseManager.shutdownAll();
   }
 }
