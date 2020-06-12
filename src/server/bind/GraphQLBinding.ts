971920de77f9cbd57e114ce042a37f90cb0fd255
tengda <kidal@vip.qq.com>
2020-06-12 14:53:50 +0800

    Release 0.19.8

--- src/server/bind/GraphQLBinding.ts
+++ src/server/bind/GraphQLBinding.ts
@@ -1,18 +1,35 @@
-import {MetadataKeys} from "./ServerBindingRegistry";
-import {Service} from "../../ioc/Annotation";
+import { MetadataKeys } from "./ServerBindingRegistry";
+import { Service } from "../../ioc/Annotation";
 
 /**
  * 设置选项
  */
-function setMetadataOption(key: string, val: any, target: Object & Function, propertyKey?: string) {
+function setMetadataOption(
+  key: string,
+  val: any,
+  target: Object & Function,
+  propertyKey?: string
+) {
   if (propertyKey) {
-    const optionsMetadata = Reflect.getMetadata(MetadataKeys.GraphQLOptions, target, propertyKey) || {}
-    optionsMetadata[key] = val
-    Reflect.defineMetadata(MetadataKeys.GraphQLOptions, optionsMetadata, target, propertyKey)
+    const optionsMetadata =
+      Reflect.getMetadata(MetadataKeys.GraphQLOptions, target, propertyKey) ||
+      {};
+    optionsMetadata[key] = val;
+    Reflect.defineMetadata(
+      MetadataKeys.GraphQLOptions,
+      optionsMetadata,
+      target,
+      propertyKey
+    );
   } else {
-    const optionsMetadata = Reflect.getMetadata(MetadataKeys.GraphQLOptions, target) || {}
-    optionsMetadata[key] = val
-    Reflect.defineMetadata(MetadataKeys.GraphQLOptions, optionsMetadata, target)
+    const optionsMetadata =
+      Reflect.getMetadata(MetadataKeys.GraphQLOptions, target) || {};
+    optionsMetadata[key] = val;
+    Reflect.defineMetadata(
+      MetadataKeys.GraphQLOptions,
+      optionsMetadata,
+      target
+    );
   }
 }
 
@@ -22,26 +39,28 @@ function setMetadataOption(key: string, val: any, target: Object & Function, pro
 export function GQLSchema(): Function {
   return (target: Function) => {
     // 标记
-    Reflect.defineMetadata(MetadataKeys.GraphQLSchema, true, target)
+    Reflect.defineMetadata(MetadataKeys.GraphQLSchema, true, target);
 
     // 注册为服务
-    return Service(target)
-  }
+    return Service(target);
+  };
 }
 
 /**
  * 声明该类是一个GraphQL的字段解析器
  * @param key 类: 类型名; 字段: 字段名.
+ * @param isSubscription 是否是订阅
  */
-export function GQLResolver(key?: string): Function {
+export function GQLResolver(key?: string, isSubscription?: boolean): Function {
   return (target: Function) => {
     // 标记
-    Reflect.defineMetadata(MetadataKeys.GraphQLResolver, true, target)
+    Reflect.defineMetadata(MetadataKeys.GraphQLResolver, true, target);
 
     // 设置key
-    setMetadataOption("key", key || target.name, target)
+    setMetadataOption("key", key || target.name, target);
+    setMetadataOption("isSubscription", isSubscription || false, target);
 
     // 注册为服务
-    return Service(target)
-  }
+    return Service(target);
+  };
 }
--- src/server/bind/ServerBindingRegistry.ts
+++ src/server/bind/ServerBindingRegistry.ts
@@ -1,25 +1,25 @@
 import * as Express from "express";
 import * as lodash from "lodash";
-import {Container} from "../../ioc/Container";
+import { Container } from "../../ioc/Container";
 import Logs from "../../application/Logs";
-import {createHandlers} from "./BoundExpressHandler";
+import { createHandlers } from "./BoundExpressHandler";
 import ReflectUtils from "../../util/ReflectUtils";
-import {GraphQLSchema} from "graphql";
-import {mergeResolver} from "../graphql/merges/merges";
+import { GraphQLSchema } from "graphql";
+import { mergeResolver } from "../graphql/merges/merges";
 import HttpServer from "../HttpServer";
 import IGraphQLContext from "../graphql/IGraphQLContext";
 
 /**
  * 允许的请求方法
  */
-export type RequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
+export type RequestMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
 /**
  * 隐射参数
  */
 interface IRequestMappingOptions {
-  path: string
-  method: string[]
+  path: string;
+  method: string[];
 }
 
 /**
@@ -57,7 +57,7 @@ export const MetadataKeys = {
   GraphQLSchema: Symbol("GQLSchema"),
   GraphQLResolver: Symbol("GraphQLResolver"),
   GraphQLOptions: Symbol("GraphQLOptions"),
-}
+};
 
 /**
  * @author tengda
@@ -66,27 +66,30 @@ export class ServerBindingRegistry {
   /**
    * 日志
    */
-  private static readonly LOG = Logs.S.getFoundationLogger(__dirname, "ServerBindingRegistry")
+  private static readonly LOG = Logs.S.getFoundationLogger(
+    __dirname,
+    "ServerBindingRegistry"
+  );
 
   /**
    * 已经注册的控制器
    */
-  private readonly registeredControllers = new Set<Function>()
+  private readonly registeredControllers = new Set<Function>();
 
   /**
    * GraphQL绑定信息
    */
-  private readonly graphQLSchema?: GraphQLSchema
+  private readonly graphQLSchema?: GraphQLSchema;
 
   /**
    * 初始化
    */
   public async init(httpServer: HttpServer, pathToScan: string) {
     // 扫描控制器文件
-    Container.addSource(pathToScan, httpServer.env.srcDir)
+    Container.addSource(pathToScan, httpServer.env.srcDir);
 
     // 注册
-    await this.registerAllUnregisteredControllers(httpServer.expressApp)
+    await this.registerAllUnregisteredControllers(httpServer.expressApp);
   }
 
   /**
@@ -95,13 +98,13 @@ export class ServerBindingRegistry {
   private async registerAllUnregisteredControllers(router: Express.Router) {
     for (const type of Container.getAllTypes()) {
       if (this.registeredControllers.has(type)) {
-        continue
+        continue;
       }
       if (!Reflect.hasMetadata(MetadataKeys.Controller, type)) {
-        continue
+        continue;
       }
-      await this.register(router, type)
-      this.registeredControllers.add(type)
+      await this.register(router, type);
+      this.registeredControllers.add(type);
     }
   }
 
@@ -111,43 +114,44 @@ export class ServerBindingRegistry {
   private retrieveRequestMappingOptions(
     target: Object & Function,
     propertyKey?: string,
-    prefix?: IRequestMappingOptions,
+    prefix?: IRequestMappingOptions
   ): IRequestMappingOptions {
     // 读取元数据
     const pathOption: string | undefined = propertyKey
       ? Reflect.getMetadata(MetadataKeys.Path, target, propertyKey)
-      : Reflect.getMetadata(MetadataKeys.Path, target)
+      : Reflect.getMetadata(MetadataKeys.Path, target);
     const methodOption: string | string[] | undefined = propertyKey
       ? Reflect.getMetadata(MetadataKeys.Method, target, propertyKey)
-      : Reflect.getMetadata(MetadataKeys.Method, target)
+      : Reflect.getMetadata(MetadataKeys.Method, target);
 
     // 路径
-    let path: string
+    let path: string;
     if (pathOption) {
       if (prefix) {
-        path = (pathOption.startsWith("/") || prefix.path.endsWith("/"))
-          ? prefix.path + pathOption
-          : prefix.path + "/" + pathOption
+        path =
+          pathOption.startsWith("/") || prefix.path.endsWith("/")
+            ? prefix.path + pathOption
+            : prefix.path + "/" + pathOption;
       } else {
-        path = pathOption
+        path = pathOption;
       }
     } else {
-      path = prefix ? prefix.path : "/"
+      path = prefix ? prefix.path : "/";
     }
     if (!path.startsWith("/")) {
-      path = "/" + path
+      path = "/" + path;
     }
 
     // 方法
-    let method: string[]
+    let method: string[];
     if (methodOption) {
-      method = typeof methodOption === "string" ? [methodOption] : methodOption
+      method = typeof methodOption === "string" ? [methodOption] : methodOption;
     } else {
-      method = prefix ? prefix.method : ["GET", "POST"]
+      method = prefix ? prefix.method : ["GET", "POST"];
     }
 
     // 完成
-    return {path, method}
+    return { path, method };
   }
 
   /**
@@ -155,69 +159,105 @@ export class ServerBindingRegistry {
    */
   private async register(router: Express.Router, type: Function) {
     // 实例化
-    const controller = Container.get(type)
+    const controller = Container.get(type);
     // 获取控制器的元数据
-    const controllerRequestMappingOptions = this.retrieveRequestMappingOptions(type)
+    const controllerRequestMappingOptions = this.retrieveRequestMappingOptions(
+      type
+    );
 
     // 获取控制器范围内的钩子
-    let beforeAllHook: Express.Handler | undefined
-    let afterAllHook: Express.Handler | undefined
-    let onErrorHook: Express.ErrorRequestHandler | undefined
-    await ReflectUtils.doWithProperties(type.prototype,
+    let beforeAllHook: Express.Handler | undefined;
+    let afterAllHook: Express.Handler | undefined;
+    let onErrorHook: Express.ErrorRequestHandler | undefined;
+    await ReflectUtils.doWithProperties(
+      type.prototype,
       async (propertyName, property) => {
         // 获取元数据
         if (!beforeAllHook) {
-          beforeAllHook = Reflect.hasMetadata(MetadataKeys.BeforeAll, type.prototype, propertyName)
-            ? property : undefined
+          beforeAllHook = Reflect.hasMetadata(
+            MetadataKeys.BeforeAll,
+            type.prototype,
+            propertyName
+          )
+            ? property
+            : undefined;
         }
         if (!afterAllHook) {
-          afterAllHook = Reflect.hasMetadata(MetadataKeys.AfterAll, type.prototype, propertyName)
-            ? property : undefined
+          afterAllHook = Reflect.hasMetadata(
+            MetadataKeys.AfterAll,
+            type.prototype,
+            propertyName
+          )
+            ? property
+            : undefined;
         }
         if (!onErrorHook) {
-          onErrorHook = Reflect.hasMetadata(MetadataKeys.OnError, type.prototype, propertyName)
-            ? property : undefined
+          onErrorHook = Reflect.hasMetadata(
+            MetadataKeys.OnError,
+            type.prototype,
+            propertyName
+          )
+            ? property
+            : undefined;
         }
       },
       async (propertyName, property) => {
-        return propertyName !== "constructor" &&
-          lodash.isFunction(property)
-      },
-    )
+        return propertyName !== "constructor" && lodash.isFunction(property);
+      }
+    );
 
     // 获取路由
-    const routes: Array<{ method: string; path: string; handlers: Express.Handler[] }> = []
-    await ReflectUtils.doWithProperties(type.prototype,
+    const routes: Array<{
+      method: string;
+      path: string;
+      handlers: Express.Handler[];
+    }> = [];
+    await ReflectUtils.doWithProperties(
+      type.prototype,
       async (propertyName, property) => {
         // 获取参数
         const requestMappingOptions = this.retrieveRequestMappingOptions(
           type.prototype,
           propertyName,
-          controllerRequestMappingOptions,
-        )
+          controllerRequestMappingOptions
+        );
 
         // 路由
         for (const method of requestMappingOptions.method) {
           const handlers = createHandlers(
-            type, controller, property, propertyName,
-            beforeAllHook, afterAllHook, onErrorHook,
-          )
-          routes.push({method, path: requestMappingOptions.path, handlers})
+            type,
+            controller,
+            property,
+            propertyName,
+            beforeAllHook,
+            afterAllHook,
+            onErrorHook
+          );
+          routes.push({ method, path: requestMappingOptions.path, handlers });
         }
       },
       async (propertyName, property) => {
-        return propertyName !== "constructor" &&
+        return (
+          propertyName !== "constructor" &&
           lodash.isFunction(property) &&
-          Reflect.hasMetadata(MetadataKeys.MappingFunction, type.prototype, propertyName)
-      },
-    )
+          Reflect.hasMetadata(
+            MetadataKeys.MappingFunction,
+            type.prototype,
+            propertyName
+          )
+        );
+      }
+    );
 
     // 注册路由
     for (const route of routes) {
-      const handlers = route.handlers.map((handler) =>
-        (req: Express.Request, res: Express.Response, next: Express.NextFunction) =>
-          Promise.resolve(handler(req, res, next)).then(null, next),
-      )
+      const handlers = route.handlers.map(
+        (handler) => (
+          req: Express.Request,
+          res: Express.Response,
+          next: Express.NextFunction
+        ) => Promise.resolve(handler(req, res, next)).then(null, next)
+      );
       const args = [route.path, ...handlers];
       (router as any)[route.method.toLocaleLowerCase()].apply(router, args);
     }
@@ -226,70 +266,88 @@ export class ServerBindingRegistry {
   /**
    * 创建GraphQL的可执行格式定义
    */
-  public async createGraphQLSchema(): Promise<{ typeDefs: string[], resolvers: any } | undefined> {
-    const typeDefs: string[] = []
-    const resolvers: any = {}
+  public async createGraphQLSchema(): Promise<
+    { typeDefs: string[]; resolvers: any } | undefined
+  > {
+    const typeDefs: string[] = [];
+    const resolvers: any = {};
 
     for (const type of Container.getAllTypes()) {
-      const isSchema: boolean = Reflect.hasMetadata(MetadataKeys.GraphQLSchema, type)
-      const isResolver: boolean = Reflect.hasMetadata(MetadataKeys.GraphQLResolver, type)
+      const isSchema: boolean = Reflect.hasMetadata(
+        MetadataKeys.GraphQLSchema,
+        type
+      );
+      const isResolver: boolean = Reflect.hasMetadata(
+        MetadataKeys.GraphQLResolver,
+        type
+      );
 
       if (isSchema) {
-        const typeDef = await this.retrieveGraphQLTypeDefs(type)
-        typeDefs.push(...typeDef)
+        const typeDef = await this.retrieveGraphQLTypeDefs(type);
+        typeDefs.push(...typeDef);
       } else if (isResolver) {
-        const [key, resolver] = await this.retrieveGraphQLTypeResolver(type)
-        mergeResolver("", resolvers, {[key]: resolver}, ServerBindingRegistry.LOG)
+        const [key, resolver] = await this.retrieveGraphQLTypeResolver(type);
+        mergeResolver(
+          "",
+          resolvers,
+          { [key]: resolver },
+          ServerBindingRegistry.LOG
+        );
       }
     }
 
-    return typeDefs.length > 0
-      ? {typeDefs, resolvers}
-      : undefined
+    return typeDefs.length > 0 ? { typeDefs, resolvers } : undefined;
   }
 
   /**
    * 创建类型定义
    */
   private async retrieveGraphQLTypeDefs(type: Function): Promise<string[]> {
-    const typeDefs: string[] = []
-    const schemaRegistry = Container.get(type)
+    const typeDefs: string[] = [];
+    const schemaRegistry = Container.get(type);
 
-    await ReflectUtils.doWithProperties(type.prototype,
+    await ReflectUtils.doWithProperties(
+      type.prototype,
       async (propertyName, property) => {
-        const results = schemaRegistry[propertyName].apply(schemaRegistry)
-        const typeDef: string = await Promise.resolve(results)
-        typeDefs.push(typeDef)
+        const results = schemaRegistry[propertyName].apply(schemaRegistry);
+        const typeDef: string = await Promise.resolve(results);
+        typeDefs.push(typeDef);
       },
       async (propertyName, property) => {
-        return propertyName !== "constructor" &&
-          lodash.isFunction(property)
-      },
-    )
+        return propertyName !== "constructor" && lodash.isFunction(property);
+      }
+    );
 
-    return typeDefs
+    return typeDefs;
   }
 
   /**
    * 创建
    */
-  private async retrieveGraphQLTypeResolver(type: Function): Promise<[string, any]> {
-    const resolverRegistry = Container.get(type)
-    const {key: typeName} = Reflect.getMetadata(MetadataKeys.GraphQLOptions, type)
-    const resolver: any = {}
+  private async retrieveGraphQLTypeResolver(
+    type: Function
+  ): Promise<[string, any]> {
+    const resolverRegistry = Container.get(type);
+    const { key: typeName, isSubscription } = Reflect.getMetadata(
+      MetadataKeys.GraphQLOptions,
+      type
+    );
+    const resolver: any = {};
 
-    await ReflectUtils.doWithProperties(type.prototype,
+    await ReflectUtils.doWithProperties(
+      type.prototype,
       async (propertyName, property) => {
-        resolver[propertyName] = async (root: any, args: any, ctx: IGraphQLContext) => {
-          return property.apply(resolverRegistry, [root, args, ctx])
-        }
+        resolver[propertyName] = isSubscription
+          ? await property.apply(resolverRegistry)
+          : async (root: any, args: any, ctx: IGraphQLContext) => {
+              return property.apply(resolverRegistry, [root, args, ctx]);
+            };
       },
       async (propertyName, property) => {
-        return propertyName !== "constructor" &&
-          lodash.isFunction(property)
-      },
-    )
+        return propertyName !== "constructor" && lodash.isFunction(property);
+      }
+    );
 
-    return [typeName, resolver]
+    return [typeName, resolver];
   }
 }
